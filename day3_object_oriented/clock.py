135d43d9a0c41ecaf32a42a474043c3bb5e78d5a
lxhello <ideal.x@qq.com>
2021-10-09 15:28:27 +0800

    a clock project

---
+++ day3_object_oriented/clock.py
@@ -0,0 +1,39 @@
+from time import sleep
+import os
+
+
+class Clock(object):
+
+    def __init__(self, hour=0, minute=0, second=0):
+        self._hour = hour
+        self._minute = minute
+        self._second = second
+
+    def run(self):
+        self._second += 1
+        if self._second == 60:
+            self._second = 0
+            self._minute += 1
+            if self._minute == 60:
+                self._minute = 0
+                self._hour += 1
+                if self._hour == 24:
+                    self._hour = 0
+
+    def show_time(self):
+        return '%02d:%02d:%02d' % (self._hour, self._minute, self._second)
+
+
+def main():
+    clock= Clock(23,59,59)
+    while True:
+        print(clock.show_time())
+        sleep(1)
+        os.system("clear")
+        clock.run()
+
+
+if __name__ == "__main__":
+    main()
+
+

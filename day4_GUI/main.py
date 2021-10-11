e47cf75c9ab0f4fcf4a52dc11fdbc2f8073a9faf
lxhello <ideal.x@qq.com>
2021-10-11 14:53:13 +0800

    add ball move

--- day4_GUI/main.py
+++ day4_GUI/main.py
@@ -44,13 +44,14 @@ def main():
     # 设置当前窗口的标题
     pygame.display.set_caption("big ball eat small ball")
     # 设置窗口的背景色(颜色是由红绿蓝三原色构成的元组)
-    screen.fill((255, 255, 255))
-    ball_image = pygame.image.load("/home/lx/pythonProject/python_100_days/day4_GUI/ball.jpg")
-    screen.blit(ball_image,(100,100))
+    # screen.fill((255, 255, 255))
+    # ball_image = pygame.image.load("/home/lx/pythonProject/python_100_days/day4_GUI/ball.jpg")
+    # screen.blit(ball_image,(100,100))
     # 绘制一个圆(参数分别是: 屏幕, 颜色, 圆心位置, 半径, 0表示填充圆)
-    pygame.draw.circle(screen, (255, 0, 0), (100, 100), 30, 0)
+    # pygame.draw.circle(screen, (255, 0, 0), (100, 100), 30, 0)
     # 刷新当前窗口(渲染窗口将绘制的图像呈现出来)
-    pygame.display.flip()
+    # pygame.display.flip()
+    x, y = 50, 50
     running = True
     # 开启一个事件循环处理发生的事件
     while running:
@@ -58,6 +59,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        screen.fill((255, 255, 255))
+        pygame.draw.circle(screen, (255, 0, 0), (x, y), 30, 0)
+        pygame.display.flip()
+        # 每隔50毫秒就改变小球的位置再刷新窗口
+        pygame.time.delay(50)
+        x, y = x + 3, y + 3
 
 
 if __name__ == "__main__":
